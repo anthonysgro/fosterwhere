f47bca52e55b0b5b57388ed6aa2655f0df1fbe2e
Anthony <AnthonySgro1995@gmail.com>
2021-05-13 14:45:12 -0400

    ltwe brute force works

--- client/helper-functions/graph-optimizations/lowTimeWithEquality.js
+++ client/helper-functions/graph-optimizations/lowTimeWithEquality.js
@@ -20,8 +20,136 @@ function findMeanAndStdDev(subGraphs) {
     return { mean, stdDev };
 }
 
-function findMeanDevOfStrip(subGraphs) {
-    console.log(subGraphs);
+function findNextSwap(subGraphs, graph) {
+    // Get a map of all commute times so we can compare
+    let graphMap = {};
+    for (const employee of graph.getEmployees()) {
+        graphMap[employee.val] = {};
+
+        for (const [clientNode, weight] of employee.neighbors.entries()) {
+            graphMap[employee.val][clientNode.val] = weight;
+        }
+    }
+
+    // Find the best mean and best standard dev to beat
+    const stats = findMeanAndStdDev(subGraphs);
+    let bestMean = stats.mean;
+    let bestStdDev = stats.stdDev;
+
+    let betterSubGraphGroup = [];
+
+    // Loops over all subgraph clients
+    for (let i = 0; i < subGraphs.length; i++) {
+        const thisSubGraph = subGraphs[i];
+        const clients = thisSubGraph.getClients();
+        const employee = thisSubGraph.getEmployees()[0];
+        // console.log(employee);
+
+        // Loops over clients of each subgraph again, comparing swaps
+        for (let j = 0; j < clients.length; j++) {
+            const client = clients[j];
+
+            // for each client, loop over all subgraphs again
+            for (let k = 0; k < subGraphs.length; k++) {
+                const comparisonGraph = subGraphs[k];
+                const compEmployee = comparisonGraph.getEmployees()[0];
+                const compClients = comparisonGraph.getClients();
+
+                if (employee.val !== compEmployee.val) {
+                    // And specifically their clients
+                    for (let l = 0; l < compClients.length; l++) {
+                        const swappable = compClients[l];
+                        const testGraph = cloneDeep(thisSubGraph);
+                        const testCompGraph = cloneDeep(comparisonGraph);
+
+                        // Get weights
+                        const empTakeWeight =
+                            graphMap[employee.val][swappable.val];
+                        const compEmpTakeWeight =
+                            graphMap[compEmployee.val][client.val];
+
+                        // Get nodes
+                        const clientNode = testGraph.findNodeByVal(client.val);
+                        const employeeNode = testGraph.findNodeByVal(
+                            employee.val,
+                        );
+                        const swappableNode = testCompGraph.findNodeByVal(
+                            swappable.val,
+                        );
+                        const compEmployeeNode = testCompGraph.findNodeByVal(
+                            compEmployee.val,
+                        );
+
+                        // Perform a test swap to see if it beats our best mean and std dev
+                        employeeNode.removeAdjacentByVal(clientNode.val);
+                        compEmployeeNode.removeAdjacentByVal(swappableNode.val);
+
+                        // employeeNode.addAdjacent(swappableNode, empTakeWeight);
+                        // compEmployeeNode.addAdjacent(
+                        //     clientNode,
+                        //     compEmpTakeWeight,
+                        // );
+
+                        testGraph.removeVertex(client.val);
+                        testCompGraph.removeVertex(swappable.val);
+
+                        testGraph.addEdge(
+                            employeeNode.val,
+                            swappableNode.val,
+                            empTakeWeight,
+                        );
+                        testCompGraph.addEdge(
+                            compEmployeeNode.val,
+                            clientNode.val,
+                            compEmpTakeWeight,
+                        );
+
+                        // We have the complete graphs! Now lets get the
+                        // others not affected by the swap so we can see
+                        // if this set is better
+                        const otherGraphs = subGraphs.filter(
+                            (sg) =>
+                                ![employee.val, compEmployee.val].includes(
+                                    sg.getEmployees()[0].val,
+                                ),
+                        );
+
+                        const { mean, stdDev } = findMeanAndStdDev([
+                            testGraph,
+                            testCompGraph,
+                            ...otherGraphs,
+                        ]);
+
+                        if (mean < bestMean && stdDev < bestStdDev) {
+                            // console.log(mean, stdDev);
+                            // console.log(employeeNode.val, swappableNode.val);
+                            // console.log(compEmployeeNode.val, clientNode.val);
+                            // console.log([testGraph, testCompGraph, lastGraph]);
+                            // console.log([
+                            //     testGraph,
+                            //     testCompGraph,
+                            //     lastGraph,
+                            // ]);
+
+                            betterSubGraphGroup = [
+                                testGraph,
+                                testCompGraph,
+                                ...otherGraphs,
+                            ];
+
+                            // bestMean = mean;
+                            // bestStdDev = stdDev;
+
+                            // return after first good one is found
+                            return betterSubGraphGroup;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    return subGraphs;
 }
 
 function lowTimeWEquality(graph) {
@@ -86,133 +214,9 @@ function lowTimeWEquality(graph) {
         subGraphs.push(subGraph);
     }
 
-    let graphMap = {};
-    for (const employee of graph.getEmployees()) {
-        graphMap[employee.val] = {};
-
-        for (const [clientNode, weight] of employee.neighbors.entries()) {
-            graphMap[employee.val][clientNode.val] = weight;
-        }
-    }
-
-    const stats = findMeanAndStdDev(subGraphs);
-    let bestMean = stats.mean;
-    let bestStdDev = stats.stdDev;
-
-    let lastChanged = null;
-    let bestSubGraphGroup = subGraphs;
-
-    let changed = false;
-
-    // Loops over all subgraph clients twice
-    for (let i = 0; i < subGraphs.length; i++) {
-        const thisSubGraph = subGraphs[i];
-        const clients = thisSubGraph.getClients();
-        const employee = thisSubGraph.getEmployees()[0];
-        // console.log(employee);
-
-        // Loops over clients of each subgraph
-        for (let j = 0; j < clients.length; j++) {
-            const client = clients[j];
-
-            // for each client, loop over all subgraphs again
-            for (let k = 0; k < subGraphs.length; k++) {
-                const comparisonGraph = subGraphs[k];
-                const compEmployee = comparisonGraph.getEmployees()[0];
-                const compClients = comparisonGraph.getClients();
-
-                if (employee.val !== compEmployee.val) {
-                    // And specifically their clients
-                    for (let l = 0; l < compClients.length; l++) {
-                        const swappable = compClients[l];
-                        const testGraph = cloneDeep(thisSubGraph);
-                        const testCompGraph = cloneDeep(comparisonGraph);
-
-                        // Get weights
-                        const empTakeWeight =
-                            graphMap[employee.val][swappable.val];
-                        const compEmpTakeWeight =
-                            graphMap[compEmployee.val][client.val];
-
-                        // Get nodes
-                        const clientNode = testGraph.findNodeByVal(client.val);
-                        const employeeNode = testGraph.findNodeByVal(
-                            employee.val,
-                        );
-                        const swappableNode = testCompGraph.findNodeByVal(
-                            swappable.val,
-                        );
-                        const compEmployeeNode = testCompGraph.findNodeByVal(
-                            compEmployee.val,
-                        );
-
-                        // Perform a test swap to see if it beats our best mean and std dev
-                        employeeNode.removeAdjacentByVal(clientNode.val);
-                        compEmployeeNode.removeAdjacentByVal(swappableNode.val);
-
-                        // employeeNode.addAdjacent(swappableNode, empTakeWeight);
-                        // compEmployeeNode.addAdjacent(
-                        //     clientNode,
-                        //     compEmpTakeWeight,
-                        // );
-
-                        testGraph.removeVertex(client.val);
-                        testCompGraph.removeVertex(swappable.val);
-
-                        testGraph.addEdge(
-                            employeeNode.val,
-                            swappableNode.val,
-                            empTakeWeight,
-                        );
-                        testCompGraph.addEdge(
-                            compEmployeeNode.val,
-                            clientNode.val,
-                            compEmpTakeWeight,
-                        );
-
-                        // We have two complete graphs! Now lets get the
-                        // last one so we can see if this set is better
-                        const lastGraph = subGraphs.filter(
-                            (sg) =>
-                                ![employee.val, compEmployee.val].includes(
-                                    sg.getEmployees()[0].val,
-                                ),
-                        )[0];
-
-                        const { mean, stdDev } = findMeanAndStdDev([
-                            testGraph,
-                            testCompGraph,
-                            lastGraph,
-                        ]);
-
-                        if (mean < bestMean && stdDev < bestStdDev) {
-                            // console.log(mean, stdDev);
-                            console.log(employeeNode.val, swappableNode.val);
-                            console.log(compEmployeeNode.val, clientNode.val);
-                            console.log([testGraph, testCompGraph, lastGraph]);
-                            // console.log([
-                            //     testGraph,
-                            //     testCompGraph,
-                            //     lastGraph,
-                            // ]);
-
-                            bestSubGraphGroup = [
-                                testGraph,
-                                testCompGraph,
-                                lastGraph,
-                            ];
-
-                            bestMean = mean;
-                            bestStdDev = stdDev;
-
-                            changed = true;
-                            subGraphs = [testGraph, testCompGraph, lastGraph];
-                            return { optimizedMap, subGraphs };
-                        }
-                    }
-                }
-            }
-        }
+    // Swap until the best swap is the same graph we are on!
+    while (subGraphs !== findNextSwap(subGraphs, graph)) {
+        subGraphs = findNextSwap(subGraphs, graph);
     }
 
     return { optimizedMap, subGraphs };
