18430a0270005804cd3f10c1d46a6ff6212fb8a9
Anthony <AnthonySgro1995@gmail.com>
2021-06-18 01:43:07 -0400

    changed order of colors

--- client/components/Map/colors.js
+++ client/components/Map/colors.js
@@ -1,24 +1,23 @@
 export default [
     { employee: "#db3434", client: "#e98585" }, // red
-    { employee: "#db8534", client: "#e9b585" }, // orange
-    { employee: "#d5db34", client: "#e7e985" }, // yellow
-    { employee: "#a3db34", client: "#c8e985" }, // light green
     { employee: "#34db4a", client: "#85e994" }, // neon green
-    { employee: "#34dba1", client: "#85e9c9" }, // seafoam
-    { employee: "#34dbcd", client: "#85e9e1" }, // teal
     { employee: "#34bfdb", client: "#85d7e9" }, // light blue
-    { employee: "#3471db", client: "#85aae9" }, // blue
-    { employee: "#4734db", client: "#9185e9" }, // indigo
+    { employee: "#db8534", client: "#e9b585" }, // orange
     { employee: "#8234db", client: "#b585e9" }, // violet
-    { employee: "#db3488", client: "#e985b9" }, // pinkish
-    { employee: "#912620", client: "#9c5a5a" }, // dark red
-    { employee: "#849120", client: "#adac61" }, // dark yellow
-    { employee: "#289120", client: "#61ad61" }, // dark green
-    { employee: "#289120", client: "#61ad61" }, // dark yellow
-    { employee: "#209180", client: "#61ada3" }, // dark cyan
-    { employee: "#205c91", client: "#618aad" }, // med blue
-    { employee: "#2b2091", client: "#6661ad" }, // dark blue
-    { employee: "#682091", client: "#9561ad" }, // dark blue
-    { employee: "#912080", client: "#ad61a3" }, // dark violet
+    { employee: "#34dba1", client: "#85e9c9" }, // seafoam
     { employee: "#7a7879", client: "#b5b3b4" }, // gray
+    { employee: "#34dbcd", client: "#85e9e1" }, // teal
+    { employee: "#4734db", client: "#9185e9" }, // indigo
+    { employee: "#d5db34", client: "#e7e985" }, // yellow
+    { employee: "#db3488", client: "#e985b9" }, // pinkish
+    { employee: "#3471db", client: "#85aae9" }, // blue
+    { employee: "#912620", client: "#9c5a5a" }, // dark red
+    { employee: "#289120", client: "#61ad61" }, // dark green
+    { employee: "#2b2091", client: "#6661ad" }, // dark blue
+    { employee: "#682091", client: "#9561ad" }, // dark something
+    { employee: "#912080", client: "#ad61a3" }, // dark violet
+    { employee: "#209180", client: "#61ada3" }, // dark cyan
+    { employee: "#849120", client: "#adac61" }, // dark yellow
+    { employee: "#205c91", client: "#618aad" }, // med blue
+    { employee: "#a3db34", client: "#c8e985" }, // light green
 ];
